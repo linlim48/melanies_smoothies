845c81ce460f4e21eb719d1172b136574cddf52a
linlim48 <166245255+linlim48@users.noreply.github.com>
2024-04-06 10:57:24 -0700

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -40,3 +39,6 @@ if ingredient_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothies is order!')
+
+cnx = st.connection('snowflake')
+session = cnx.session()

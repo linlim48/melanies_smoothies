bd8cbae46c2419204ad0a0b22d12373e1fd5e532
linlim48 <166245255+linlim48@users.noreply.github.com>
2024-04-06 11:45:32 -0700

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -15,8 +15,9 @@ st.write('The name on your Smoothie will be', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width= True)
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredient_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -29,6 +30,7 @@ if ingredient_list:
     
     for fruit_chosen in ingredient_list:
             ingredients_string += fruit_chosen + ' '
+            st.subheader(fruit_chosen + 'Nutrition Information')
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         

d6f341808045bd6b34f9e3b09d5fb78217e449a4
linlim48 <166245255+linlim48@users.noreply.github.com>
2024-04-06 11:50:15 -0700

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -20,8 +20,8 @@ my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT
 #st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
 st.stop()
 
 ingredient_list = st.multiselect(
@@ -35,6 +35,9 @@ if ingredient_list:
     
     for fruit_chosen in ingredient_list:
             ingredients_string += fruit_chosen + ' '
+
+            search_on=pf_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+            st.write('The search value for ', fruit_chose, ' is ', search_on, '.')
             st.subheader(fruit_chosen + 'Nutrition Information')
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
